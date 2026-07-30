7718364ea75c440412b7af8e2e33542c9bb74b8d
amitisrael20 <123574985+amitisrael20@users.noreply.github.com>
2026-07-30 15:07:25 +0300

    making a rectungle to get face with coords+ libaraies update

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ import cv2
 
 camera = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
 while True:
     success, frame = camera.read()
 
     if not success:
         break
 
-    cv2.imshow("Camera", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray_frame,
+        scaleFactor=1.1,
+        minNeighbors=5
+    )
+
+    for x, y, width, height in faces:
+        cv2.rectangle(
+            frame,
+            (x, y),
+            (x + width, y + height),
+            (0, 255, 0),
+            2
+        )
+
+    cv2.imshow("Face Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
